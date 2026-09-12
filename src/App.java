7ab30ec594af52b0da69800413e9c585794a2fdc
CaballeroUrrego <urregosebastian462@gmail.com>
2026-09-12 03:14:53 -0500

    feat: 12/09/2026 Estructuras de control interativas : do while

--- src/App.java
+++ src/App.java
@@ -1,13 +1,19 @@
 public class App {
         public static void main(String[] args) throws Exception {
-                // 12/09/2026 Estructuras de control interativas :while
-                int contador = 1;
+                // 12/09/2026 Estructuras de control interativas : do while
+                int contador = 6;
+                do{
+                
+                System.out.println("Esto sucede antes de la condicion");
+                System.out.println(contador);
+                //Actualizacion de la variable
+                contador++;
 
-                while (contador <= 5) {
+
+                }while (contador <= 5); {
                         System.out.println(contador);
-                        // una actualizacion de la variable para evitar un bucle infinito
-                        contador++;
+                        
                 }
-                System.err.println(contador);
+
         }
 }
