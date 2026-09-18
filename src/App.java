7ae2d5e4cdb037be48745d595070f33d5a13a766
CaballeroUrrego <urregosebastian462@gmail.com>
2026-09-18 01:31:44 -0500

    constructores, uso de this y sobrecarga de constructores

--- src/App.java
+++ src/App.java
@@ -1,38 +1,19 @@
 public class App {
         public static void main(String[] args) throws Exception {
-                Persona persona1 = new Persona();
-                persona1.Nombre = "Leonardo";
-                persona1.Apellido = "Dicaprio";
-                persona1.Edad = 25;
-                // Carrera de Leonardo
-                Carrera carrera1 = new Carrera();
-                carrera1.nombre = "Ingenieria en computacion";
-                carrera1.duracion = 6;
-                carrera1.estaEnCurso = false;
 
-                persona1.carrera = carrera1;
-                // Creación del segundo objeto independiente (persona2)
-                Persona persona2 = new Persona();
-                persona2.Nombre = "Mariana";
-                persona2.Apellido = "Alvarez";
-                persona2.Edad = 46;
-                // Carrera de Mariana
-                Carrera carrera2 = new Carrera();
-                carrera2.nombre = "Ingenieria en sistemas";
-                carrera2.duracion = 6;
-                carrera2.estaEnCurso = true;
-                persona2.carrera = carrera2;
-                String saludado = " Desarollador Urrego";
-                // Invocación del método darNombreCompleto() y lectura de atributos
+                Persona persona1 = new Persona("pedro", "Pascal", 40, "Mandaloriano", 5, true);
+
+                /*
+                 * persona1.Nombre = "Patricio";
+                 * persona1.Apellido = "Sosa";
+                 * persona1.Edad = 60;
+                 * // Carrera de Leonardo
+                 * persona1.carrera.nombre = "Musica";
+                 */
 
                 // persona 1
-                System.out.println(persona1.darNombreCompleto() + ", " + "tiene " + persona1.Edad + " años y esta recibido de " + persona1.carrera.nombre);
-                // persona 2
-                System.out.println(persona2.darNombreCompleto() + ", " + "tiene " + persona2.Edad
-                                + " años y eta cursando " + persona2.carrera.nombre);
-
-                // System.out.println(persona1.enviarSaludo(saludado));
-                // System.out.println(persona2.enviarSaludo(" Desarollador"));
+                System.out.println(persona1.darNombreCompleto() + ", " + "tiene " + persona1.Edad
+                                + " años y esta recibida de " + persona1.carrera.nombre);
 
         }
 
--- src/Persona.java
+++ src/Persona.java
@@ -6,6 +6,37 @@ public class Persona {
   // Clase carrera es decir un atributo
   Carrera carrera;
 
+  // Constructor
+  // los constructores son bloques de codigo que permiten inicializar los
+  // atributos de la clase al momento de crear un objeto.
+  // tienen el mismo nombre de la clase
+  /*------------------------------------------------------ */
+  // this :Se utiliza para acceder a variables de instancias o metodos de las
+  // clases actuales y puede ser util para referirse a las propiedades propias de
+  // la clase actual osea variables de instancia o de clase, si no se usa se puede
+  // crear variables con el mismo nombre que los parametros de entrada sin generar
+  // confunsion
+
+  public Persona(String nombre, String apellido, int edad, String nombreCarrera, int duracionCarrera,
+      boolean estaEnCurso) {
+    carrera = new Carrera(nombreCarrera, duracionCarrera, estaEnCurso);
+    this.Nombre = nombre;
+    this.Apellido = apellido;
+    this.Edad = edad;
+
+  }
+
+  // tambien podemos hacer un constructor que reciba mucho menos parameros
+  // esto es una sobrecarga de constructor con el mismo nombre pero que tenga
+  // comportamientos distintos
+  public Persona(String nombre, String apellido, int edad, String nombreCarrera) {
+    carrera = new Carrera(nombreCarrera);
+    this.Nombre = nombre;
+    this.Apellido = apellido;
+    this.Edad = edad;
+
+  }
+
   // Metodos Son los comportamientos de un objeto
 
   public String darNombreCompleto() {
