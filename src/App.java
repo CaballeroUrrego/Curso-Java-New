0802e4089a002cac0ff5564acb2a978be74a7c43
CaballeroUrrego <urregosebastian462@gmail.com>
2026-09-18 02:22:15 -0500

    Encapsulamiento : getters y setters
    Pasar los atributos de una clase a privados que solamente puedan ser accedidos a traves de los getters y setters

--- src/App.java
+++ src/App.java
@@ -1,20 +1,13 @@
 public class App {
         public static void main(String[] args) throws Exception {
 
-                Persona persona1 = new Persona("pedro", "Pascal", 40, "Mandaloriano", 5, true);
-
-                /*
-                 * persona1.Nombre = "Patricio";
-                 * persona1.Apellido = "Sosa";
-                 * persona1.Edad = 60;
-                 * // Carrera de Leonardo
-                 * persona1.carrera.nombre = "Musica";
-                 */
-
+                Persona persona1 = new Persona("pedro", "Pascal", 60);
+                persona1.setNombre("Ricardo");
+                persona1.setApellido("Darin");
                 // persona 1
-                System.out.println(persona1.darNombreCompleto() + ", " + "tiene " + persona1.Edad
-                                + " años y esta recibida de " + persona1.carrera.nombre);
-
+                // System.out.println(persona1.darNombreCompleto() + ", " + "tiene "
+                // + persona1.Edad + " años");
+                System.out.println(persona1.darNombreCompleto() + ", " + "tiene " + persona1.getEdad() + " años");
         }
 
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,36 +1,13 @@
 public class Persona {
   // Atributos y caracterizticas de un objeto
-  String Nombre;
-  String Apellido;
-  int Edad;
-  // Clase carrera es decir un atributo
-  Carrera carrera;
+  // private hace que los atributos sean privados y solo se puedan acceder desde
+  // la clase
+  private String Nombre;
+  private String Apellido;
+  private int Edad;
 
   // Constructor
-  // los constructores son bloques de codigo que permiten inicializar los
-  // atributos de la clase al momento de crear un objeto.
-  // tienen el mismo nombre de la clase
-  /*------------------------------------------------------ */
-  // this :Se utiliza para acceder a variables de instancias o metodos de las
-  // clases actuales y puede ser util para referirse a las propiedades propias de
-  // la clase actual osea variables de instancia o de clase, si no se usa se puede
-  // crear variables con el mismo nombre que los parametros de entrada sin generar
-  // confunsion
-
-  public Persona(String nombre, String apellido, int edad, String nombreCarrera, int duracionCarrera,
-      boolean estaEnCurso) {
-    carrera = new Carrera(nombreCarrera, duracionCarrera, estaEnCurso);
-    this.Nombre = nombre;
-    this.Apellido = apellido;
-    this.Edad = edad;
-
-  }
-
-  // tambien podemos hacer un constructor que reciba mucho menos parameros
-  // esto es una sobrecarga de constructor con el mismo nombre pero que tenga
-  // comportamientos distintos
-  public Persona(String nombre, String apellido, int edad, String nombreCarrera) {
-    carrera = new Carrera(nombreCarrera);
+  public Persona(String nombre, String apellido, int edad) {
     this.Nombre = nombre;
     this.Apellido = apellido;
     this.Edad = edad;
@@ -40,13 +17,40 @@ public class Persona {
   // Metodos Son los comportamientos de un objeto
 
   public String darNombreCompleto() {
-    return Apellido + ", " + Nombre;
+    // return Apellido + ", " + Nombre;
+    return getApellido() + ", " + getNombre();
+
   }
 
   public String enviarSaludo(String saludado) {
     if (Edad > 40)
       return "Buenos dias,querido" + saludado;
     return "Hola, ¿como estas" + saludado + "?";
+  }
+
+  // Getters y Setters //Sirven para obtener y modificar el valor de un atributo
+  // privado
+  // Forma manual de obtener el valor de un atributo privado
+  // Getters
+  public int getEdad() {
+    return this.Edad;
+  }
+
+  public String getNombre() {
+    return this.Nombre.toUpperCase();
+  }
+
+  public String getApellido() {
+    return this.Apellido.toUpperCase();
+  }
+
+  // Setters
+  public void setNombre(String nombre) {
+    this.Nombre = nombre;
+  }
+
+  public void setApellido(String apellido) {
+    this.Apellido = apellido;
   }
 
 }
